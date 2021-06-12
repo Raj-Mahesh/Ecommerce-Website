f077ec362ea6203626f47c9cff73a4acc2e77e00
Raj-Mahesh <raj.mahesh@freshworks.com>
2021-06-12 16:01:51 +0530

    Adding Frontend Part

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+function Footer() {
+    return (
+        <footer>
+            <Row>
+                <Col className="text-center py-3">Copyright &copy; Ecommerce</Col>
+            </Row>
+        </footer>
+    )
+}
+
+export default Footer
